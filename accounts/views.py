c0b8baa806e01d20e0919f8ad2455fd617fc64a4
Aditya Bisen <adityabisen@Adityas-MacBook-Air.local>
2022-07-26 01:42:44 +0530

    login logout added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login
 # Create your views here.
+def index(request):
+    print(request.user)
+    if request.user.is_anonymous:
+        return redirect('/login')
+    else:
+        return render(request,'index.html')
+
+
+def loginuser(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username,password)
+
+        user = authenticate(username=username, password=password)
+        print(request.user)
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request, user)
+            return redirect("/")
+        else:
+            # No backend authenticated the credentials
+            return render(request,'login.html')
+
+    return render(request,'login.html')
+
+def logoutuser(request):
+    logout(request)
+    return redirect('/login')
