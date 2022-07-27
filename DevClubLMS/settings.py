e3e48b38dbb1d676017ab28321b6554b9ca7fc92
Aditya Bisen <adityabisen@Adityas-MacBook-Air.local>
2022-07-27 20:45:54 +0530

    static file management

--- DevClubLMS/settings.py
+++ DevClubLMS/settings.py
@@ -125,4 +125,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login'
 
-STATIC_ROOT = None
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
